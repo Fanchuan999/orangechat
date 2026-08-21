fcb71db2b658b9bb6e8e0a5b835bc8a3a2bcb7fc
Fanchuan999 <1206692376@qq.com>
2026-08-21 19:22:00 +0800

    build: bump Daddy companion update to v226

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "me.rerere.orangechat"
         minSdk = 26
         targetSdk = 37
-        versionCode = 225
-        versionName = "2.5.39"
+        versionCode = 226
+        versionName = "2.5.40"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
