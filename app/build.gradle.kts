6f7d3cd8820e833f3be95c11e9eb634d575bd0e9
Fanchuan999 <1206692376@qq.com>
2026-08-28 17:41:31 +0800

    chore(release): bump companion to 2.5.44

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "me.rerere.orangechat"
         minSdk = 26
         targetSdk = 37
-        versionCode = 229
-        versionName = "2.5.43"
+        versionCode = 230
+        versionName = "2.5.44"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
